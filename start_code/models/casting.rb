652dcfa1448eaea0d890dafab8441aabad20da0c
Ted Osuch <ted@osuch.com>
2019-08-15 14:22:56 +0100

    sql & classes defined

--- start_code/models/casting.rb
+++ start_code/models/casting.rb
@@ -1,3 +1,15 @@
+require_relative('../db/sql_runner')
+
 class Casting
+
+  attr_reader :id, :movies_id, :stars_id
+
+  def   initialize(options)
+    @id = options['id'].to_i if options['id']
+    @movies_id = options['movies_id']
+    @stars_id = options['stars_id']
+    @fee = options['fee']
+  end
+
 
 end
